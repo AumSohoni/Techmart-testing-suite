1a88b120bf7d1dc5dea823b3681c571098e3c957
batman1110Aum <aumsohoni11@gmail.com>
2026-08-23 01:18:15 +0300

    accesiblity tests

--- Tests/e2e/accesiblity.spec.ts
+++ Tests/e2e/accesiblity.spec.ts
@@ -1,8 +1,27 @@
-import {test ,expect, Page} from '@playwright/test';
+import { test, expect, Page } from '@playwright/test';
+
+
 
 /*
-
     ACCESSIBILITY TESTS
 
-*/
 
+*/ 
+test.describe("Accessibility Tests", () => {
+
+
+test.describe("All images should have alt text", () => {
+
+    test("Check for images without alt text", async ({ page }) => {
+        await page.goto('/index.html');
+        const imagesWithoutAlt = await page.$$eval('img:not([alt])', imgs => imgs.length);
+        expect(imagesWithoutAlt).toBe(0);
+    });
+
+
+
+
+
+
+
+});
